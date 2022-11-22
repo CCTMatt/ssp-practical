b4675249d5c1cf292cefec4dea3e9d38658d58f5
CCTMatt <2021390@student.cct.ie>
2022-11-22 16:05:24 +0000

    Copying from mikhail

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const http = require('http'), //HTTP server
-      express = require('express'), //Handlinf HTTP requests and routing
-      fs = require('fs'),
-      xmlParse = require('xslt-processor').xmlParse,
-      xsltProcess = require('xslt-processor').xsltProcess,
-      router = express(),
-      server = http.createServer(router);
+const   http = require('http'), //HTTP server
+        path = require('path'),
+        express = require('express'), //Handling HTTP requests & routing
+        fs = require('fs'), //File system functionalities
+        xmlParse = require('xslt-processor').xmlParse, //XML handling
+        xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
+        router = express(), //Init our router
+        server = http.createServer(router); //Init our server
+        
+        router.use(express.static(path.resolve(__dirname,'views'))); 
 
-router.get('/', function(req, res){
+router.get('/', function(req, res) {
+
     res.writeHead(200, {'Content-Type' : 'text/html'});
 
     let xml = fs.readFileSync('menu.xml', 'utf8'),
         xsl = fs.readFileSync('menu.xsl', 'utf8');
 
     xml = xmlParse(xml);
-    xsl = xslParse(xsl);
+    xsl = xmlParse(xsl);
 
     let html = xsltProcess(xml, xsl);
 
     res.end(html.toString());
 });
 
-server.listen(process.env.Port || 300, process.env.IP || "0.0.0.0", function()
-{
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
     const addr = server.address();
     console.log("Server listening at", addr.address + ":" + addr.port)
 });
